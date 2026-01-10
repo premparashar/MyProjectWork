da8a82d8b7e64fb003a4982a4c790c237b455069
premparashar <103783576+premparashar@users.noreply.github.com>
2026-01-10 17:51:07 -0500

    Going to commit my project

---
+++ src/gitproj/GitClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package gitproj;
+
+/**
+ *
+ * @author Prem Parashar
+ * Date: 2026-01-10
+ */
+public class GitClass {
+    public String name;
+    public String dept;
+
+}
---
+++ src/gitproj/GitProj.java
@@ -0,0 +1,16 @@
+package gitproj;
+
+/**
+ 
+ * @author Prem Parashar
+ */
+public class GitProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
