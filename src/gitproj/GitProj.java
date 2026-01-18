3e4be8785a82d10e5d6a89a7672ca04b7082a0a3
premparashar <103783576+premparashar@users.noreply.github.com>
2026-01-18 12:31:20 -0500

    Update GitProj.java

--- src/gitproj/GitProj.java
+++ src/gitproj/GitProj.java
@@ -10,7 +10,7 @@ public class GitProj {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Add something here
+        System.out.println("hello World ");
     }
 
 }
